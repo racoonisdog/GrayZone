9e13f3788778c68bf44e663790c0f89ab43dbe21
Kimjunghyeon <130948037+racoonisdog@users.noreply.github.com>
2026-07-09 14:44:13 +0900

    Fixed - 오류 나던거 수정

---
+++ Assets/1.Scripts/Temp/PlayerAnimationEventReceiver.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class PlayerAnimationEventReceiver : MonoBehaviour
+{
+    public void OnFootstep(AnimationEvent animationEvent)
+    {
+
+    }
+
+    public void OnLand(AnimationEvent animationEvent)
+    {
+
+    }
+}
